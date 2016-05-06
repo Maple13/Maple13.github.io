8c8060e25429762a058f512e6e98e9016a3283de
Maple13 <Mr_daijc@163.com>
2016-05-06 12:26:06 +0800

    Site updated: 2016-05-06 12:26:01

---
+++ resume/php/contact.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2015-11-11 17:58:18
+ * @version $Id$
+ */
+
+class ClassName extends AnotherClass {
+
+    function __construct(){
+
+    }
+}
